3c68143d0390cf7ec3ec1d764efaed67af0dd487
gfixler <gary@garyfixler.com>
2017-12-26 11:48:39 -0800

    Exercise 2.1 (Easy) - circleArea

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -2,9 +2,19 @@ module Main where
 
 import Prelude ((+), (*))
 import Control.Monad.Eff.Console (logShow)
-import Math (sqrt)
+import Math (pi, sqrt)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
+
+
+-- 1. (Easy) Use the pi constant, which is defined in the Math module, to write
+-- a function circleArea which computes the area of a circle with a given
+-- radius. Test your function using PSCi (Hint: don’t forget to import pi by
+-- modifying the import Math statement).
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r
+
 
 main = logShow (diagonal 3.0 4.0)
