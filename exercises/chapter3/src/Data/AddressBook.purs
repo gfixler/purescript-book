a09417028d4fba2af0329cdf5dcb94acd659dcdf
gfixler <gary@garyfixler.com>
2017-12-27 11:06:37 -0800

    Exercise 3.1.4 (Difficult) - remove dupe names

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -3,7 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head, null)
+import Data.List (List(..), filter, head, nubBy, null)
 import Data.Maybe (Maybe)
 
 type Address =
@@ -70,3 +70,15 @@ nameInAddressBook firstName lastName = not <<< null <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+
+-- 4. (Difficult) Write a function removeDuplicates which removes duplicate
+-- address book entries with the same first and last names. Hint: Use PSCi to
+-- find the type of the Data.List.nubBy function, which removes duplicate
+-- elements from a list based on an equality predicate.
+
+removeDupeEntries :: AddressBook -> AddressBook
+removeDupeEntries = nubBy isDupe
+    where
+    isDupe :: Entry -> Entry -> Boolean
+    isDupe a b = a.firstName == b.firstName && a.lastName == b.lastName
