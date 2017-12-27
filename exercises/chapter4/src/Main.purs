4d1522d7b290f99e08ce390c2fbc0f4d573c1ad3
gfixler <gary@garyfixler.com>
2017-12-27 12:31:35 -0800

    Exercise 4.1.2 (Medium) - count evens

--- exercises/chapter4/src/Main.purs
+++ exercises/chapter4/src/Main.purs
@@ -2,6 +2,10 @@ module Main where
 
 import Prelude
 
+import Data.Array (null)
+import Data.Array.Partial (head, tail)
+import Partial.Unsafe (unsafePartial)
+
 
 -- 4.1.1. (Easy) Write a recursive function which returns true if and only if
 -- its input is an even integer.
@@ -19,3 +23,15 @@ isEven' = go true
     go true 0 = true
     go false 0 = false
     go b n = go (not b) (n - 1)
+
+
+-- 4.1.2. (Medium) Write a recursive function which counts the number of even
+-- integers in an array. Hint: the function unsafePartial head (where head is
+-- also imported from Data.Array.Partial) can be used to find the first element
+-- in a non-empty array.
+
+countEvens :: Array Int -> Int
+countEvens arr =
+    if null arr
+        then 0
+        else (if isEven (unsafePartial head arr) then 1 else 0) + countEvens (unsafePartial tail arr)
