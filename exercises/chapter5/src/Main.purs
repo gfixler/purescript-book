0a2a02410d05bc39cd530d30eecb5e532f078721
gfixler <gary@garyfixler.com>
2018-01-13 22:30:49 -0800

    Exercise 5.1.2 (Medium) - n choose k
    I used this page to better understand Pascal's Rule:
    https://artofproblemsolving.com/wiki/index.php?title=Pascal%27s_Identity

--- exercises/chapter5/src/Main.purs
+++ exercises/chapter5/src/Main.purs
@@ -9,3 +9,12 @@ import Prelude
 fac :: Int -> Int
 fac 1 = 1
 fac n = n * fac (n - 1)
+
+-- 5.1.2 (Medium) Look up Pascal’s Rule for computing binomial coefficients.
+-- Use it to write a function which computes binomial coefficients using
+-- pattern matching.
+
+choose :: Int -> Int -> Int
+choose _ 0 = 1
+choose n k | n < k  = 0
+           | otherwise = choose (n-1) (k-1) + choose (n-1) k
